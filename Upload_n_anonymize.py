22ec996527d9b3442a0b77e5162a6e2bbaba47c2
kumarkushagra <kumarkushagra777@gmail.com>
2024-07-18 23:01:02 +0530

    Fixed anonymization function

--- Upload_n_anonymize.py
+++ Upload_n_anonymize.py
@@ -4,13 +4,13 @@ import csv
 
 
 # Uploading begins here
-def upload_zip_file(zip_paths_arr):
+def upload_zip_file(zip_paths_arr,csv_file_path):
     if type(zip_paths_arr) != list:
         zip_paths_arr = [zip_paths_arr]
     orthanc_url = "http://localhost:8042"
 
+    befor_uploading = requests.get(f"{orthanc_url}/studies").json()
     for zip_file_path in zip_paths_arr:
-        print(f"Uploading ZIP file: {zip_file_path}")
         # Read the ZIP file in binary mode
         with open(zip_file_path, 'rb') as f:
             zip_data = f.read()
@@ -19,42 +19,28 @@ def upload_zip_file(zip_paths_arr):
         orthanc_url_with_instances = orthanc_url.rstrip('/') + '/instances'
         response = requests.post(orthanc_url_with_instances, data=zip_data, headers={'Content-Type': 'application/zip'})
         response.raise_for_status()
-        print(f"Uploaded ZIP file: {zip_file_path}")
+    after_uploading = requests.get(f"{orthanc_url}/studies").json()
+
 
     # Anonymization begins here
-
-    print("Fetching list of new uploaded studies for anonymization...")
-    csv_file_path = 'Orthanc_uploaded_StudyID.csv'
-    uploaded_list = []
-    if os.path.exists(csv_file_path):
-        with open(csv_file_path, mode='r', newline='') as file:
-            csv_reader = csv.DictReader(file)
-            for row in csv_reader:
-                uploaded_list.append(row['Uploaded'])
-    
-    new_studies_list = requests.get(f"{orthanc_url}/studies").json()
-    new_studies = list(set(new_studies_list) - set(uploaded_list))
-    
+    new_studies = list(set(after_uploading) - set(befor_uploading))
     print(f"Anonymizing {len(new_studies)} new studies...")
     anonymize_response = requests.post(
         f"{orthanc_url}/tools/bulk-anonymize",
         json={"Resources": new_studies}
     )
     anonymize_response.raise_for_status()
-    print("Anonymization complete.")
+
 
     # Delete original studies from Orthanc PACS
-    print("Deleting original studies from Orthanc PACS...")
     for study_id in new_studies:
         study_delete_url = f'{orthanc_url}/studies/{study_id}'
         delete_response = requests.delete(study_delete_url)
         delete_response.raise_for_status()
-    print("Original studies deleted.")
 
     all_studies = requests.get(f"{orthanc_url}/studies").json()
-    new_studies = list(set(all_studies) - set(uploaded_list))
+    new_studies = list(set(all_studies) - set(befor_uploading))
 
-    print("Updating the CSV file with uploaded studies...")
     fieldnames = ['Uploaded']
     data = []
     concatenated_string = ''.join(new_studies)
@@ -71,7 +57,6 @@ def upload_zip_file(zip_paths_arr):
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(data)
-    print("CSV file updated with uploaded studies.")
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -10,12 +10,21 @@ from delete_temp_zip import delete_temp_files
 
 
 def main():
+    # Hard code the initial parameters
     dcm4chee_url = "http://13.235.102.234:8080"
-    csv_file_path = "Uploaded_dcm4chee.csv"
+    csv_file_path = "Logs/Uploaded_dcm4chee.csv"
     download_path = "./Temp_Downloads"
     source_url = "http://localhost:8042"
     destination_url = "http://localhost:1111"
-    
+
+
+    # ensure that nessecary directory exist
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+    if not os.path.exists("Logs"):
+        os.makedirs("Logs")
+
+
     # Downloading
     all_studies = fetch_study_ids(dcm4chee_url)
     uploaded_studies = get_uploaded_study_ids(csv_file_path)
@@ -28,11 +37,14 @@ def main():
     dicom_paths = list_zip_files()
 
     # Uploading and anonymizing data
-    upload_zip_file(dicom_paths)
-    pacs_to_pacs_transfer(source_url, destination_url)
+    upload_zip_file(dicom_paths,csv_file_path)
 
     # deleting temporary files
     delete_temp_files()
+
+    # Transfering data
+    pacs_to_pacs_transfer(source_url, destination_url)
+
 
 if __name__ == "__main__":
     main()
